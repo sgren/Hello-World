f075c3ff9e5435ffd94937677a7be14e803975fc
sgren <sgren@users.noreply.github.com>
2018-12-18 08:20:17 +0100

    Add files via upload

---
+++ Öppna projekt i versionshanteraren.c
@@ -0,0 +1,17 @@
+1.	Skapa en lämplig katalog under work med rätt projektnamn
+2.	Öppna en browser till adressen:
+	https://opconsrv004.opcon.se:8443/svn/
+3.	Välj det önskade projektet i repositories collection.
+
+Skapa ett nytt repository
+1. Logga in via fjärrskrivbordet på servern
+2. Starta Visual Server SVN manager
+3. Högerklicka på repositories i vänsterfönstret
+4. Välj Create New Repository
+5. Ange namn
+6. Kryssa i Create Default Structure
+7. Välj OK
+8. Välj File exit
+9. Välj Log Off på fjärrskrivbordet
+
+	
